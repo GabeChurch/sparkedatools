7aac3f2e258b44f791c6933ec5a6272b42cbb562
root <root@edge01.abyss.net>
2019-05-01 20:41:38 -0500

    added numeric auto-inference for improving ggplot numeric scaling on hist_overlay

--- R/spark_hist_overlay.R
+++ R/spark_hist_overlay.R
@@ -14,18 +14,28 @@
 #' @export 
 spark_hist_overlay = function(sparklyr_table, response_var){
   library(ggplot2)
-  raw_df = sparklyr_table %>% spark_dataframe()
+  library(purrr)
+  library(sparklyr)
+  numericcharacters = function(x) {
+    !any(is.na(suppressWarnings(as.numeric(x)))) & is.character(x)
+  }
   
+  raw_df = sparklyr_table %>% spark_dataframe()
   collected = sparklyr::invoke_new(sc, "com.gabechurch.sparklyRWrapper") %>% 
     sparklyr::invoke("histResponse", raw_df, response_var) %>%
     sparklyr::invoke("collect")
-  returned = (1:length(collected)) %>% map(function(i){
+  (1:length(collected)) %>% map(function(i){
     current_column = collected[i] %>% flatten
     column_name = paste((current_column[2] %>% flatten), collapse=', ')
     target_name = paste((current_column[3] %>% flatten), collapse=', ')
-    barValues = current_column[4] %>% flatten %>% map(function(row_list) {
+    column_type = paste((current_column[4] %>% flatten), collapse=', ')
+    barValues = current_column[5] %>% flatten %>% map(function(row_list) {
       cur_hist_bar = (row_list %>% flatten)
-      bar_name = cur_hist_bar[1]
+      bar_name = if (column_type == 'numeric'){
+        as.numeric(cur_hist_bar[1]) %>% as.list()
+      }else{
+        cur_hist_bar[1]
+      }
       tar_bar_name = cur_hist_bar[2]
       bar_counts = cur_hist_bar[3]
       names(cur_hist_bar)[1] = paste0(column_name)
@@ -33,19 +43,20 @@ spark_hist_overlay = function(sparklyr_table, response_var){
       names(cur_hist_bar)[3] = "Counts"
       cur_hist_bar
     })
-    prepped = barValues %>% map(flatten) %>% bind_rows() %>% as.data.frame()
-    
+    prepped = barValues %>% map(flatten) %>% bind_rows() %>% as.data.frame() %>% 
+      mutate_if(numericcharacters,as.numeric) #%>%
+    prepped[,paste0(target_name)] = as.factor(prepped[,paste0(target_name)])
+    #Begin ggplot
     ggplot(data=prepped, 
            aes_string(x=paste0(column_name), 
                       y='Counts',
-                      fill=paste0(target_name))) + 
-      
-      geom_bar(stat="identity", width=0.75) + 
-      theme(axis.text.x = element_text(angle=60, hjust=1, vjust=0.5, size=11),
-            axis.text.y = element_text(size=11),
-            axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0), size=15),
-            axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0), size=15)
+                      fill=paste0(target_name))
+            ) + 
+            geom_bar(stat="identity", width=0.75) +  
+            theme(axis.text.x = element_text(angle=60, hjust=1, vjust=0.5, size=11),
+                  axis.text.y = element_text(size=11),
+                  axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0), size=15),
+                  axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0), size=15)
       ) 
   })
-  returned
 }
